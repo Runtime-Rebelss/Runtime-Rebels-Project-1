1792b22c1d81e28a9728abc509ce1da7ff70766e
Henry <henry.locke.763@my.csun.edu>
2025-10-01 10:18:49 -0700

    Added Getters and Setters

--- Runtime-Rebels-Project-1/spring-boot/src/main/java/com/project1/spring_boot/models/Cart.java
+++ Runtime-Rebels-Project-1/spring-boot/src/main/java/com/project1/spring_boot/models/Cart.java
@@ -1,35 +1,60 @@
 package com.project1.spring_boot.models;
 
-import jakarta.persistence.*;
-import java.util.*;
+import jakarta.persistence.Entity;
 
 @Entity(name="CART")
 public class Cart {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-
-    @ManyToOne
-    @JoinColumn(name="customer_id")
-    private User customer;
+    // Create private ints and Strings for the Product, name, desc, price, available
+    private int productId;
+    private String productName;
+    private String productDescription;
+    private int productPrice;
+    private int quantityAvailable;
 
     public Cart() {
     }
 
-    public int getId() {
-        return id;
+    public int getProductId() {
+        return productId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setProductId(int productId) {
+        this.productId = productId;
     }
 
-    public User getCustomer() {
-        return customer;
+    public String getProductName() {
+        return productName;
     }
 
-    public void setCustomer(User customer) {
-        this.customer = customer;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(int quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+
+
+
 }
