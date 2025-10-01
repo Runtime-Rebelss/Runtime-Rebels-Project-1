33222f76e8f73f16a9c6a721a1620a57ad6a0171
Henry <henry.locke.763@my.csun.edu>
2025-10-01 09:56:05 -0700

    Updated code in ShoppingCartApplication.java

--- Runtime-Rebels-Project-1/spring-boot/src/main/java/com/project1/spring_boot/ShoppingCartApplication.java
+++ Runtime-Rebels-Project-1/spring-boot/src/main/java/com/project1/spring_boot/ShoppingCartApplication.java
@@ -7,14 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
-        //ApplicationContext ctx = SpringApplication.run(ShoppingCart.class, args);
         SpringApplication.run(ShoppingCartApplication.class, args);
-        // System.out.println("Shopping Cart");
-
-        // String[] beanNames = ctx.getBeanDefinitionNames();
-        // Arrays.sort(beanNames);
-        // for (String beanName : beanNames) {
-        // 	System.out.println(beanName);
-        // }
     }
 }
+
