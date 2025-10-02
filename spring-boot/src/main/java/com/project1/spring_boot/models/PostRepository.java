66c16176670b38b2084892ab02fb3dfc5827be50
Henry <henry.locke.763@my.csun.edu>
2025-10-02 13:42:41 -0700

    Cleaned Code

--- spring-boot/src/main/java/com/project1/spring_boot/models/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project1.spring_boot.models;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-// Data you want to get and key 
-public interface PostRepository extends MongoRepository<Post, String>{
-    
-}
