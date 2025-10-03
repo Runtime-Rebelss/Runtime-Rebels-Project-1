efd4c8dd3c871a04f276802d1e1e0882f48fd14a
Haley Kenney <haleykenney102002@gmail.com>
2025-10-03 11:47:27 -0700

    Added ProductDetails React setup

--- backend/src/main/java/com/project1/spring_boot/models/Product.java
+++ backend/src/main/java/com/project1/spring_boot/models/Product.java
@@ -12,9 +12,9 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+    private String imageUrl;
 
-    public String getId() {
-        return id;
+    public String getId() {return id;
     }
     public void setId(String id) {
         this.id = id;
@@ -36,6 +36,13 @@ public class Product {
     }
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
 }
