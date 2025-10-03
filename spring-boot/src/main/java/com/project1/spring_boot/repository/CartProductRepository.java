270bdd9b8a1c4c18a8a8c36095b36251dc9dfef2
Frank Gonzalez <frank.c.gonzalez.03@gmail.com>
2025-10-03 16:50:22 -0700

    Prepared main for merge with product-details-haley

--- spring-boot/src/main/java/com/project1/spring_boot/repository/CartProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project1.spring_boot.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.project1.spring_boot.models.Cart;
-
-public interface CartProductRepository extends  MongoRepository<Cart, Integer> {
-
-}
