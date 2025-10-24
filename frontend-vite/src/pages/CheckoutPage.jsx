d263866f9583e271605a32d7080d2a35aad02dd3
Frank Gonzalez <frank.c.gonzalez.03@gmail.com>
2025-10-24 13:49:50 -0700

    Fixed checkoutImage import to find image in assets folder.

--- frontend-vite/src/pages/CheckoutPage.jsx
+++ frontend-vite/src/pages/CheckoutPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { ShoppingBag, Trash2, Plus, Minus } from 'lucide-react';
 import Navbar from '../components/Navbar';
 import cartLib from '../lib/cart';
-import checkoutImage from '../components/Scamazon_Coming_Soon.png';
+import checkoutImage from '../assets/Scamazon_Coming_Soon.png';
 const CheckoutPage = () => {
     return (<div className="max-w-6xl mx-auto p-6 flex flex-col lg:flex-row gap-10">
         <div className="flex-1 flex items-center justify-center">
